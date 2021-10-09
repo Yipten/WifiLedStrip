c967e9b6dea06f0b997795674d767fed306f6cac
Yipten <yipten@outlook.com>
2021-10-09 17:17:43 -0400

    fix all colors & larger font

--- WifiLedStrip.ino
+++ WifiLedStrip.ino
@@ -13,11 +13,11 @@ const char* password = PASSWORD;
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(NUM_LEDS, PIN, NEO_GRB + NEO_KHZ800);
 uint32_t off = strip.Color(0, 0, 0);
 uint32_t red = strip.Color(255, 0, 0);
-uint32_t orange = strip.Color(0, 255, 63);
-uint32_t yellow = strip.Color(0, 255, 127);
+uint32_t orange = strip.Color(255, 63, 0);
+uint32_t yellow = strip.Color(255, 127, 0);
 uint32_t green = strip.Color(0, 255, 0);
 uint32_t blue = strip.Color(0, 0, 255);
-uint32_t purple = strip.Color(255, 255, 0);
+uint32_t purple = strip.Color(255, 0, 255);
 uint32_t white = strip.Color(255, 255, 255);
 
 void handleNotFound() {
@@ -47,7 +47,7 @@ void handleRoot() {
 		strip.setPixelColor(i, *rgb);
 	strip.show();
 	// send HTTP response
-	server.send(200, "text/html", "<form action=\".\" method=\"get\">	<button type=\"submit\" value=\"0\" name=\"color\">off</button><br>	<button type=\"submit\" value=\"1\" name=\"color\">red</button><br>	<button type=\"submit\" value=\"2\" name=\"color\">green</button><br>	<button type=\"submit\" value=\"3\" name=\"color\">blue</button>	</form>");
+	server.send(200, "text/html", "<h1>	<form action=\".\" method=\"get\">	<button type=\"submit\" value=\"0\" name=\"color\" style=\"font-size: 96px\">off</button><br>	<button type=\"submit\" value=\"1\" name=\"color\" style=\"font-size: 96px\">red</button><br>	<button type=\"submit\" value=\"2\" name=\"color\" style=\"font-size: 96px\">green</button><br>	<button type=\"submit\" value=\"3\" name=\"color\" style=\"font-size: 96px\">blue</button>	</form>	</h1>");
 }
 
 void setup() {
